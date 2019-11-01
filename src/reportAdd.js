cfbc942046938d95e0130c8d83bb8fe027a3c4c5
Lars Persson <lars.persson6@learnet.se>
2019-11-01 19:29:34 +0100

    Fixing code structure

--- src/reportAdd.js
+++ src/reportAdd.js
@@ -23,81 +23,69 @@ class Addsreports extends React.Component {
         this.handleChange = this.handleChange.bind(this);
     }
 
-handleChange = (event) => {
-  event.preventDefault();
-  const { name, value } = event.target;
-  this.setState({[name]: value});
-}
+    handleChange = (event) => {
+        event.preventDefault();
+        const { name, value } = event.target;
+        this.setState({[name]: value});
+    }
 
 
-handleSubmit = (event) => {
-  event.preventDefault();
-  // console.log(this.state.week)
-  // console.log(this.state.description)
-  //   console.log(this.state.texten)
-      reports.addReport(this.state.week, this.state.description, this.state.texten);
-      this.setState({reportSended: "sended"});
-    //   return  <Redirect  to="/reports" />
-    // auth.login();
-    // console.log(auth.token);
-}
+    handleSubmit = (event) => {
+        event.preventDefault();
+        reports.addReport(this.state.week, this.state.description, this.state.texten);
+        this.setState({reportSended: "sended"});
+    }
 
 
-render() {
-  // console.log(<Login/>)
-  // console.log(reports.res.data.message);
-  if (this.state.reportSended) {
-    return <Redirect to="/reports/week" />;
-  }
+    render() {
+        if (this.state.reportSended) {
+            return <Redirect to="/reports/week" />;
+        }
 
+        return (
 
+            <div>
+            <h3>Rapport</h3>
+            <form onSubmit={this.handleSubmit} >
 
-  return (
-
-    <div>
-
-
-        <h3>Rapport</h3>
-        <form onSubmit={this.handleSubmit} >
-
-          <div className='email'>
+            <div className='email'>
             <p>Vecka
             {this.state.week.length === 0 ?
-                 <span className='inforeport'>  - Obligatorisk fält </span>  :
-              <span className='valid'>&#10004;</span>} </p>
-            <input type='number' name='week' onChange={this.handleChange} required/>
+                <span className='inforeport'>  - Obligatorisk fält </span>  :
+                <span className='valid'>&#10004;</span>} </p>
+                <input type='number' name='week' onChange={this.handleChange} required/>
+                </div>
 
-              </div>
+                <div className='email'>
+                <p>Rubrik
+                {this.state.description.length === 0 ?
+                    <span className='inforeport'>  - Obligatorisk fält </span>  :
+                    <span className='valid'>&#10004;</span>} </p>
+                    <input type='text' name='description'  required="required" onChange={this.handleChange}  />
+                    </div>
 
-              <div className='email'>
-              <p>Rubrik
-            {this.state.description.length === 0 ?
-                 <span className='inforeport'>  - Obligatorisk fält </span>  :
-              <span className='valid'>&#10004;</span>} </p>
-            <input type='text' name='description'  required="required" onChange={this.handleChange}  />
-              </div>
-              <div className='email'>
-              <p>Texten
-            {this.state.texten.length === 0 ?
-                 <span className='inforeport'>  - Obligatorisk fält </span>  :
-              <span className='valid'>&#10004;</span>} </p>
-            <textarea name='texten' required="required" onChange={this.handleChange} />
-              </div>
+                    <div className='email'>
+                    <p>Texten
+                    {this.state.texten.length === 0 ?
+                        <span className='inforeport'>  - Obligatorisk fält </span>  :
+                        <span className='valid'>&#10004;</span>} </p>
+                        <textarea name='texten' required="required" onChange={this.handleChange} />
+                        </div>
 
-              {this.state.texten.length !== 0 && this.state.description.length !== 0 && this.state.week.length !== 0 ?
-                    <div className='submit'>
-                       <button onClick={this.handleSubmit}>Registrera</button>
-                     </div> :
-                     <div className='submit'>
-                        <button disabled>Registrera</button>
-                      </div>
+                        {this.state.texten.length !== 0 && this.state.description.length !== 0 && this.state.week.length !== 0 ?
+                            <div className='submit'>
+                            <button onClick={this.handleSubmit}>Registrera</button>
+                            </div> :
+                            <div className='submit'>
+                            <button disabled>Registrera</button>
+                            </div>
+                        }
+
+                        </form>
+
+                        </div>
+                    );
                 }
-
-        </form>
-
-    </div>
-  );
-}
 }
 
 export default ReportAdd;
